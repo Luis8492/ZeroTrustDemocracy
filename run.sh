8befa944a7c0bdebea6984894bdc120141a55f12
Luis8492 <130156566+Luis8492@users.noreply.github.com>
2025-08-28 21:56:50 +0900

    scroll fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cd app;../../bin/python3 minute_fetcher.py;../../bin/python3 minute_analyzer.py;../../bin/python3 -m uvicorn feederAPI:app --host 127.0.0.1 --port 8000 & # API起動
-cd ../frontend;../../bin/python3 -m http.server 8001 --bind 127.0.0.1
+cd app;../../bin/python3 minute_fetcher.py;../../bin/python3 minute_analyzer.py;../../bin/python3 -m uvicorn feederAPI:app --host localhost --port 8000 & # API起動
+cd ../frontend;../../bin/python3 -m http.server 8001 --bind localhost
