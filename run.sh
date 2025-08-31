8e22e8776df305e6520ab178889d09413e124802
Luis8492 <130156566+Luis8492@users.noreply.github.com>
2025-08-31 18:27:49 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-cd app;../../bin/python3 fetch.py --municipality setagaya;../../bin/python3 minute_analyzer.py;../../bin/python3 -m uvicorn feederAPI:app --host localhost --port 8000 & # API起動
-cd ../frontend;../../bin/python3 -m http.server 8001 --bind localhost
+../bin/python3 fetch.py --municipality setagaya;
+
+cd app;
+../../bin/python3 minute_analyzer.py;
+../../bin/python3 -m uvicorn feederAPI:app --host localhost --port 8000 & # API起動
+
+cd ../frontend;
+../../bin/python3 -m http.server 8001 --bind localhost
